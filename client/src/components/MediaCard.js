f62040aee1add5375098d1ac3e7bbd978e10e6b5
Jordan FOKOUA <jfokoua@gmail.com>
2020-04-05 15:39:19 +0200

    :zap: Optimize media card component

--- client/src/components/MediaCard.js
+++ client/src/components/MediaCard.js
@@ -5,40 +5,81 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345
+    maxWidth: 345,
   },
   media: {
-    height: 300
-  }
+    height: 300,
+  },
 });
 
-const MediaCard = ({ imgURL, title, index }) => {
+const MediaCard = ({
+  imgURL,
+  index,
+  results,
+  candidate,
+  voters,
+  voteAction,
+  voted
+}) => {
   const classes = useStyles();
+  console.log("candidate", candidate)
+  console.log("voters", voters)
 
   return (
     <Card className={classes.root}>
       <CardActionArea>
-        <CardMedia className={classes.media} image={imgURL} title={title} />
+        <CardMedia
+          className={classes.media}
+          image={imgURL}
+          title={candidate.title}
+        />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            {`${index + 1} - ${title}`}
+            {`${index + 1} - ${candidate.name}`}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Typography>
+          {results ? (
+            <Box>
+              <Typography variant="h4" color="textSecondary" component="p">
+                {voters > 0
+                  ? `${(candidate.voteCount / voters) * 100} %`
+                  : "0%"}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {" "}
+                {`${candidate.voteCount} votes`}
+              </Typography>
+            </Box>
+          ) : (
+            <>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur.
+              </Typography>
+            </>
+          )}
         </CardContent>
       </CardActionArea>
+      {voteAction && (
+        <IconButton
+          onClick={() => voteAction()}
+          color="primary"
+          aria-label="Vote"
+          disabled={voted}
+        >
+          <ThumbUpIcon fontSize="large" />
+        </IconButton>
+      )}
     </Card>
   );
 };
